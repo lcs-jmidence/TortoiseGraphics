320f8ec6afb4a6377bb2e909aa8915d235afbf53
Julio Midence <jmidence@lcs.on.ca>
2018-11-03 17:53:05 -0400

    Finished my name, I'll try to work on the signature

--- Playground/Playground.playground/Pages/Cursive Name.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/Cursive Name.xcplaygroundpage/Contents.swift
@@ -14,30 +14,22 @@ PlaygroundPage.current.liveView = canvas
 
 // Start drawing
 canvas.drawing { t in
-    t.penSize(3)
+    
     //Start turtle at top left corner
     t.penUp()
-    t.forward(150)
-    t.left(90)
-    t.forward(200)
-    //Upercase J
-    t.penDown() 
-    //Loop Up
-    t.curve(withSides: 30, withSize: 15, drawSides: 5)
-    //Turn around
-    t.curve(withSides: 60, withSize: 1, drawSides: 20)
-    //Moving down
-    t.curve(withSides: 60, withSize: 7, drawSides: 10)
-    //Bottom Loop
-    t.curve(withSides: 40, withSize: 6, drawSides: 4)
-    t.curve(withSides: -20, withSize: 7, drawSides: 5)
-    t.curve(withSides: -15, withSize: 6, drawSides: 7)
-    t.curve(withSides: 30, withSize: 10, drawSides: 7)
-    //show heading
-    print("Heading after J is \(t.heading)")
-    print("Y position after J is \(t.ycor)")
-    //Hide
+    t.goto(-200, 150)
+    //set pen width
+    t.penSize(4)
     t.hideTortoise()
+    
+    //Draw my First Name
+    t.uppercaseJ()
+    //lowercase u
+    //initial half-circle
+    t.right(105)
+    t.curve(withSides: -14, withSize: 10, drawSides: 9)
+    
+    
 }
 
 
--- Sources/Cursive Name.swift
+++ Sources/Cursive Name.swift
@@ -1,4 +1,23 @@
 public extension Tortoise {
-   
+    func uppercaseJ() {
+        //Upercase J
+        self.penDown()
+        self.setHeading(-10)
+        //Loop Up
+        self.curve(withSides: 30, withSize: 15, drawSides: 5)
+        //Turn around
+        self.curve(withSides: 60, withSize: 1, drawSides: 20)
+        //Moving down
+        self.curve(withSides: 60, withSize: 7, drawSides: 10)
+        //Bottom Loop
+        self.curve(withSides: 40, withSize: 6, drawSides: 4)
+        self.curve(withSides: -20, withSize: 7, drawSides: 5)
+        self.curve(withSides: -15, withSize: 6, drawSides: 7)
+        self.curve(withSides: 30, withSize: 10, drawSides: 7)
+    }
+    
+    func lowercaseu() {
+        
+    }
     
 }
